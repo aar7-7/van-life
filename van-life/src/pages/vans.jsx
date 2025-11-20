748713abcd94006971add1c4c3987dcb6db406fb
aar7-7 <aliasgharrahimi34@gmail.com>
2025-11-20 23:15:05 +0330

    Refactor Vans component to improve data handling and rendering; enhance styling for better layout and responsiveness

--- van-life/src/pages/vans.jsx
+++ van-life/src/pages/vans.jsx
@@ -1,24 +1,38 @@
 import React from 'react'
 import '../style/van.css'
 export default function Vans() {
-    const [data,setData] = React.useState(null)
-    React.useEffect((()=>{
-        fetch("/api/vans")
-          .then((res) => res.json())
-          .then((data) => setData(data));
-    }),[])
-    console.log(data)
-  return (
-    <div className="vans-page">
-      <h1>Explore our Van options</h1>
-      <div className="cat-section">
-        <div className='filter'>
-          <div className="cat">Simple</div>
-          <div className="cat">Luxury</div>
-          <div className="cat">Rugged</div>
+    const [data, setData] = React.useState([]);
+    React.useEffect(() => {
+      fetch("/api/vans")
+        .then((res) => res.json())
+        .then((data) => setData(data.vans));
+    }, []);
+    const vanElements = data.map((van) => (
+      <div key={van.id} className="van-tile">
+        <img src={van.imageUrl} alt={van.name} />
+        <div className="van-info">
+          <h3>{van.name}</h3>
+          <p>
+            ${van.price}
+            <span>/day</span>
+          </p>
         </div>
-        <div className="clear-filter">Clear filters</div>
+        <i className={`van-type ${van.type}`}>{van.type}</i>
       </div>
-    </div>
-  );
+    ));
+
+    return (
+      <div className="vans-page">
+        <h1>Explore our Van options</h1>
+        <div className="cat-section">
+          <div className="filter">
+            <div className="cat">Simple</div>
+            <div className="cat">Luxury</div>
+            <div className="cat">Rugged</div>
+          </div>
+          <div className="clear-filter">Clear filters</div>
+        </div>
+        <div className="vans">{vanElements}</div>
+      </div>
+    );
 }
